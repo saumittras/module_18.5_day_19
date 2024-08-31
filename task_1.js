d8eeecb8f9e1378294f1570f071fad1c12ae1112
saumittras <saumittra.eee@gmail.com>
2024-08-31 21:01:45 +0600

    Added Array Task 1 and its working successfully

---
+++ task_1.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+ */
+
+const fruits = ["Apple", "Banana", "Jackfruit", "Mango", "strawberry"];
+console.log(fruits[2]);
+fruits[2] = "Jambura";
+console.log(fruits);
