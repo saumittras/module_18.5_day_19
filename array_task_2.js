1a1539ae480ff8ae3eb79aac83563c6e3ca4cc77
saumittras <saumittra.eee@gmail.com>
2024-08-31 21:14:49 +0600

    Added array task 2 and it is working well

---
+++ array_task_2.js
@@ -0,0 +1,17 @@
+/**
+ * 2. Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output
+ * 
+ * 
+ */
+
+const touristDestinations = ["Sundarban", "Bandarban", "Cox's Bazar"];
+touristDestinations.push("Rangamati");
+touristDestinations.push("Sajek Valley", "kuakata");
+console.log(touristDestinations);
+touristDestinations.pop();
+console.log(touristDestinations);
