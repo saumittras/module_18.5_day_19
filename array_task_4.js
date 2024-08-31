8364e9bbc00b579bed5e042e92fb7b1f1c27edd3
saumittras <saumittra.eee@gmail.com>
2024-08-31 21:43:24 +0600

    added array task 4 and working it successfully

---
+++ array_task_4.js
@@ -0,0 +1,19 @@
+/**
+ *4. Checking if it's an Array
+Instructions:
+
+Create different variables, each containing either an array or a non-array value.
+
+Now use isArray to check if each variable is an array.
+
+Print a message to the console indicating whether each variable is an array or not.
+ *
+ */
+
+const var1 = "hi";
+const var2 = [];
+const var3 = 5;
+
+console.log(Array.isArray(var1));
+console.log(Array.isArray(var2));
+console.log(Array.isArray(var3));
